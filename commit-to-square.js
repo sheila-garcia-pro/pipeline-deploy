bee377fe4e60ef4ff563e67a0b4ec949f2b63d2b
Thomas Lins <thomaslins43@gmail.com>
2025-04-14 23:29:03 -0300

    fix: teste

--- commit-to-square.js
+++ commit-to-square.js
@@ -5,7 +5,8 @@ import { dirname } from "path";
 
 const token = process.env.SQUARE_API_KEY;
 const api_id = process.env.API_ID_KEY;
-const api = new SquareCloudAPI(token);
+console.log(token, api_id)
+const api = new SquareCloudAPI("24de303b047c1b32d2c1fcec42ed0e1bc6fe74f3-4e3da8a6b5aeb8f83af1ae318c232888f7286fef7e6c71696c63b5d9fe4ad2b0");
 const app = await api.applications.get("8075952c25f74e918cc365966da4e6d4");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
